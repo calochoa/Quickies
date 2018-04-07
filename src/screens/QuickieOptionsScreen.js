2bad14fa13bf3f55cc5a86b245293de7644d163a
Christian Ochoa <calochoa@gmail.com>
2018-04-07 02:15:50 -0700

    add overlay for quickie options; add quickie options / body split json

--- src/screens/QuickieOptionsScreen.js
+++ src/screens/QuickieOptionsScreen.js
@@ -6,10 +6,20 @@ import {
   Image,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import QuickieOptions from '../dbstore/QuickieOptions.json';
 import MenuIcon from '../components/MenuIcon';
+import InfoIcon from '../components/InfoIcon';
 import ForwardIcon from '../components/ForwardIcon';
 import MainContainerStyle from '../style/MainContainerStyle';
 import MainRowStyle from '../style/MainRowStyle';
+import OverlayStyle from '../style/OverlayStyle';
+import Overlay from 'react-native-modal-overlay';
+
+
+const QuickieOptionsMap = {};
+QuickieOptions.map(element => {
+  QuickieOptionsMap[element.qoName] = element;
+})
 
 
 class QuickieOptionsScreen extends React.Component {
@@ -28,23 +38,40 @@ class QuickieOptionsScreen extends React.Component {
     };
   };
 
-  renderRow(section) {
+  constructor(props) {
+    super(props);
+
+
+    let sectionTitles = []
+    QuickieOptions.map(element => {
+      sectionTitles.push(element.qoName);
+    });
+
+    this.state = {
+      sectionTitles: sectionTitles,
+    };
+  }
+
+  setModalVisible(sectionTitle, visible) {
+    const update = {}
+    update[sectionTitle] = visible
+    this.setState(update);
+  }
+
+  renderOverlay(sectionTitle) {
     return (
-      <LinearGradient 
-        colors={['#4c669f', '#0276c9', '#192f6a']} 
-        style={MainRowStyle.container} 
-        key={section.title}
+      <Overlay 
+        visible={this.state[sectionTitle]}
+        closeOnTouchOutside={true}
+        containerStyle={OverlayStyle.container}
+        childrenWrapperStyle={OverlayStyle.wrapper}
+        onClose={() => {this.setModalVisible(sectionTitle, false);}}
       >
-        <View style={MainRowStyle.titleContainer}>
-          <Text style={MainRowStyle.title}>{section.title}</Text>
-        </View>
-        <View style={MainRowStyle.nextLevelContainer}>
-          <TouchableOpacity onPress={() => {this.props.navigation.navigate(section.screen);}}>
-            <ForwardIcon />
-          </TouchableOpacity>
-        </View>
-      </LinearGradient>
-    );
+        <Text style={OverlayStyle.header}>{QuickieOptionsMap[sectionTitle].qoHeader}</Text>
+        <View style={OverlayStyle.divider}/>
+        <Text style={OverlayStyle.text}>{QuickieOptionsMap[sectionTitle].qoDescription}</Text>
+      </Overlay>
+    )
   }
 
   renderRow(section, image) {
@@ -54,6 +81,13 @@ class QuickieOptionsScreen extends React.Component {
         style={MainRowStyle.container} 
         key={section.title}
       >
+        {this.renderOverlay(section.title)}
+        <TouchableOpacity 
+          style={MainRowStyle.infoLevelContainer} 
+          onPress={() => {this.setModalVisible(section.title, true);}}
+        >
+          <InfoIcon />
+        </TouchableOpacity>
         <View style={MainRowStyle.titleContainer}>
           <Text style={MainRowStyle.title}>
             <View style={{marginRight: 5}}>
@@ -77,7 +111,7 @@ class QuickieOptionsScreen extends React.Component {
         {this.renderRow({title: 'All', screen: 'Quickies', extra: {quickieType: 'All'}})}
         {this.renderRow({title: 'by Level', screen: 'QuickieLevels'})}
         {this.renderRow({title: 'by Type', screen: 'QuickieTypes'})}
-        {this.renderRow({title: 'by Muscle Group', screen: 'QuickieTypes'})}
+        {this.renderRow({title: 'by Body Split', screen: 'QuickieTypes'})}
         {this.renderRow(
           {title: 'Favorite', screen: 'QuickieTypes'}, 
           <Image source={require('../images/icons8-star-26-gold.png')} />
